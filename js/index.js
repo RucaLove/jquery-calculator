892325c57569c8e01079c9efe53aa9fb0983743d
Haley Kalb <haleykalb@Haleys-MacBook-Air.local>
2017-02-20 15:16:48 -0700

    calculator working

---
+++ js/index.js
@@ -0,0 +1,140 @@
+  // 1. Inputs: #(operands: string), operators, C (clear), =, button clicking
+  //  Outputs: total, screen
+  // Whatifs: start w/ops, no ops, too many ops, op probs, too many #'s, %0 errors,
+  // 2. Divise a plan: E.L. for span to print to screen (class !op)
+
+  $(document).ready(function() {
+    let screen = $('#screen')
+    let n1 = '';
+    // let n2 = '';
+    let operator = '';
+    let result = '';
+    let space = ' ';
+    let newNum1 = '';
+    let arrNum1 = [];
+    let repeatOp = false;
+    // let nextEquation = false;
+    // console.log("SCREEN", $(screen));
+
+    $('span').click(function(event) {
+      let operand = $(event.target).text()
+      // console.log("Array Num 1", arrNum1);
+
+      //if it is not an 'operator'
+      if (!$(this).hasClass("operator")) {
+        repeatOp = true;
+        //If screen contains a zero, take out the zero before inputing new number
+        if (screen.text() == 0) {
+          screen.text("");
+        }
+        n1 += operand;
+        // console.log("n1", n1);
+        screen.append(operand);
+
+        //if it is an 'operator'
+      } else if ($(this).hasClass("operator") && n1) {
+        // console.log("I'm here....");
+
+        // if (arrNum1.length === 0) {
+        //   screen.text("Error!")
+        // }
+
+        if (n1 != "") {
+          arrNum1.push(n1);
+          // console.log("ArrNum", arrNum1);
+        }
+        // console.log(arrNum1);
+        if (this.innerText !== '=') {
+          // console.log("TYPE OF OPERATOR", operator);
+          // console.log("THIS HTML typeof length", this.innerText.length);
+
+          operator = this.innerHTML;
+        }
+
+        screen.append(space + operator + space);
+        n1 = '';
+
+        if ($(event.target).attr("id") === "clear") {
+          arrNum1 = [];
+          n1 = "";
+          screen.text("0");
+        }
+
+
+        if ($(event.target).attr("id") === "equals") {
+          // console.log("I clicked equals");
+          // console.log("O", operator);
+
+          // let operator =
+          switch (operator) {
+            case '+':
+              result = +(arrNum1[0]) + +(arrNum1[1])
+              screen.text(result)
+              // console.log("RESULT +", result);
+              arrNum1 = [];
+              arrNum1.push(result);
+              break;
+            case '-':
+              result = +(arrNum1[0]) - +(arrNum1[1])
+              screen.text(result)
+              // console.log("RESULT -", result);
+              arrNum1 = [];
+              arrNum1.push(result);
+              break;
+            case 'x':
+              result = +(arrNum1[0]) * +(arrNum1[1])
+              screen.text(result)
+              // console.log("RESULT *", result);
+              arrNum1 = [];
+              arrNum1.push(result)
+              break;
+            case '÷':
+              result = +(arrNum1[0]) / +(arrNum1[1])
+              screen.text(result)
+              // console.log("RESULT ÷", result);
+              arrNum1 = [];
+              arrNum1.push(result)
+              break;
+            default:
+              return "whoops";
+          }
+        }
+        // console.log("MY EVENT TARGET", $(event.target).text());
+      }
+
+    })
+  })
+
+  // $(document).ready(function() {
+  //    let screen = $('#screen')
+  //    var keys = ''
+    //  var secondOp = ''
+    //  var thirdOp = ''
+    //  var total = ''
+    //  var operator = ''
+    // $('span').click(function(event){
+       // console.log(event.target);
+       // firstOp += $(event.target).text();
+       // screen.text(firstOp);
+    //  if (!$(event.target).hasClass('operator')) {
+    //    keys += $(event.target).text();
+    //    screen.text(keys);
+       //!== ('.operator')
+       // screen.append(firstOp)
+    //  }
+    //  if ($(event.target).hasClass('operator') && !$(event.target).attr('id') === 'clear' && !$(equal)) {
+    //   screen.text(operator)
+    //  }
+    //  if ($(event.target).hasClass('operator') && $(event.target).attr('id') === 'clear' && $(event.target).attr('id') === 'equals') {
+    //    console.log("HI");
+    //  }
+      //  if ($("span:contains('C')")) {
+      //    screen.text('');
+        //  $('#screen').text(' ');
+        //  display = '';
+        //  operator = '';
+      //  }
+
+
+ //   })
+ // })
